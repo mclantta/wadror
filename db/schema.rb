5ec7cdd7c0227bb8e4afa38954a0cd437ae155c0
Maija Lantta <maija.lantta@cs.helsinki.fi>
2016-02-07 22:02:58 +0200

    assign. 7-8

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206195741) do
+ActiveRecord::Schema.define(version: 20160207180747) do
+
+  create_table "beer_clubs", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "founded"
+    t.string   "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "beers", force: :cascade do |t|
     t.string   "name"
