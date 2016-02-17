185abb5eac4a0ebc10cb40fe88cbd115ec891dde
Maija Lantta <maija.lantta@cs.helsinki.fi>
2016-02-17 22:29:27 +0200

    week5, a: 5 and 6

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,11 +2,18 @@ class PlacesController < ApplicationController
   def index
   end
 
+  def show
+    @place_id = params[:id]
+    @places = BeermappingApi.places_in(session[:last_city])
+  end
+
  def search
     @places = BeermappingApi.places_in(params[:city])
     if @places.empty?
     	redirect_to places_path, notice: "No locations in #{params[:city]}" and return
     end
+    session[:last_city] = "#{params[:city]}"
+    #session[:last_id] = "#{@places.id}"
     render :index
  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   get 'signin', to: 'sessions#new'
   delete 'signout', to: 'sessions#destroy'
-  get 'places', to: 'places#index'
+  #get 'places', to: 'places#index'
+  #get 'places/:id', to:'places#show'
   post 'places', to:'places#search'
   
   # wanhoja
@@ -30,9 +31,11 @@ Rails.application.routes.draw do
   #   resources :products
   resources :ratings, only: [:index, :new, :create]
   resources :ratings, only: [:index, :new, :create, :destroy]
+  resources :places, only:[:index, :show]
 
   resource :session, only: [:new, :create, :destroy]
 
+  
   # Example resource route with options:
   #   resources :products do
   #     member do
